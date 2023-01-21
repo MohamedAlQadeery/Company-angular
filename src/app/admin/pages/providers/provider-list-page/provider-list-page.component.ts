765bbcc0e0d633b7a25f73e50c5794d147cfffc0
MohamedAlQadeery <moh9amad1@gmail.com>
2023-01-21 18:59:49 +0200

    remove primeng

--- src/app/admin/pages/providers/provider-list-page/provider-list-page.component.ts
+++ src/app/admin/pages/providers/provider-list-page/provider-list-page.component.ts
@@ -1,66 +1,8 @@
-import { DecimalPipe } from '@angular/common';
-import { Component, PipeTransform } from '@angular/core';
-import { FormControl } from '@angular/forms';
-import { Observable, map, startWith } from 'rxjs';
-
-interface Country {
-  name: string;
-  flag: string;
-  area: number;
-  population: number;
-}
-
-const COUNTRIES: Country[] = [
-  {
-    name: 'Russia',
-    flag: 'f/f3/Flag_of_Russia.svg',
-    area: 17075200,
-    population: 146989754,
-  },
-  {
-    name: 'Canada',
-    flag: 'c/cf/Flag_of_Canada.svg',
-    area: 9976140,
-    population: 36624199,
-  },
-  {
-    name: 'United States',
-    flag: 'a/a4/Flag_of_the_United_States.svg',
-    area: 9629091,
-    population: 324459463,
-  },
-  {
-    name: 'China',
-    flag: 'f/fa/Flag_of_the_People%27s_Republic_of_China.svg',
-    area: 9596960,
-    population: 1409517397,
-  },
-];
-
-function search(text: string, pipe: PipeTransform): Country[] {
-  return COUNTRIES.filter((country) => {
-    const term = text.toLowerCase();
-    return (
-      country.name.toLowerCase().includes(term) ||
-      pipe.transform(country.area).includes(term) ||
-      pipe.transform(country.population).includes(term)
-    );
-  });
-}
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-provider-list-page',
   templateUrl: './provider-list-page.component.html',
   styleUrls: ['./provider-list-page.component.css'],
 })
-export class ProviderListPageComponent {
-  countries$: Observable<Country[]>;
-  filter = new FormControl('', { nonNullable: true });
-
-  constructor(pipe: DecimalPipe) {
-    this.countries$ = this.filter.valueChanges.pipe(
-      startWith(''),
-      map((text) => search(text, pipe))
-    );
-  }
-}
+export class ProviderListPageComponent {}
