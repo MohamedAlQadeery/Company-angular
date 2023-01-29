a5bb66b1d14f4bef68c86b7a0f28081e954c868c
MohamedAlQadeery <moh9amad1@gmail.com>
2023-01-30 01:56:26 +0200

    created offer creation form

--- src/app/site/pages/profile/profile-offers-create-page/profile-offers-create-page.component.ts
+++ src/app/site/pages/profile/profile-offers-create-page/profile-offers-create-page.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { LangService } from 'src/app/services/language.service';
 
 @Component({
   selector: 'app-profile-offers-create-page',
   templateUrl: './profile-offers-create-page.component.html',
-  styleUrls: ['./profile-offers-create-page.component.css']
+  styleUrls: ['./profile-offers-create-page.component.css'],
 })
-export class ProfileOffersCreatePageComponent {
+export class ProfileOffersCreatePageComponent implements OnInit {
+  constructor(private _langService: LangService) {}
+  currentLang$ = this._langService.currentLang$;
 
+  //#region Form Controls
+  offerFormGroup: FormGroup;
+  descriptionControl = new FormControl('', [Validators.required]);
+  photoControl = new FormControl('', [Validators.required]);
+  //#endregion
+  ngOnInit(): void {
+    this.offerFormGroup = new FormGroup({
+      description: this.descriptionControl,
+      photo: this.photoControl,
+    });
+  }
+
+  HandleOnSubmit() {
+    console.log(this.offerFormGroup.value);
+  }
 }
