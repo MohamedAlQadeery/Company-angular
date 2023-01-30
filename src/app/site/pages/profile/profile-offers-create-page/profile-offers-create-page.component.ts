1a58e79b6b90321a4031dc7363a647e8eb13e378
MohamedAlQadeery <moh9amad1@gmail.com>
2023-01-30 13:40:22 +0200

    creating offer page in profile

--- src/app/site/pages/profile/profile-offers-create-page/profile-offers-create-page.component.ts
+++ src/app/site/pages/profile/profile-offers-create-page/profile-offers-create-page.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
+import { ToastrService } from 'ngx-toastr';
 import { LangService } from 'src/app/services/language.service';
+import { OfferService } from 'src/app/services/offer.service';
 
 @Component({
   selector: 'app-profile-offers-create-page',
@@ -8,7 +12,13 @@ import { LangService } from 'src/app/services/language.service';
   styleUrls: ['./profile-offers-create-page.component.css'],
 })
 export class ProfileOffersCreatePageComponent implements OnInit {
-  constructor(private _langService: LangService) {}
+  constructor(
+    private _langService: LangService,
+    private _translate: TranslateService,
+    private _offerService: OfferService,
+    private _toastr: ToastrService,
+    private _router: Router
+  ) {}
   currentLang$ = this._langService.currentLang$;
 
   //#region Form Controls
@@ -24,6 +34,18 @@ export class ProfileOffersCreatePageComponent implements OnInit {
   }
 
   HandleOnSubmit() {
-    console.log(this.offerFormGroup.value);
+    let messageTitle = this._translate.instant('add-offer');
+    let messageContent = this._translate.instant('add-offer-success');
+    this._offerService.CreateOffer(this.offerFormGroup.value).subscribe({
+      next: (res) => {
+        console.log(res);
+        this._toastr.success(messageContent, messageTitle);
+
+        this._router.navigate(['/profile/offers']);
+      },
+      error: (err) => {
+        this._toastr.error(err);
+      },
+    });
   }
 }
