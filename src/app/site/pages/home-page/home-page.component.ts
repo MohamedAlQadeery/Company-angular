69d3532ea21815f424ac2112763df083b1e37fdf
MohamedAlQadeery <moh9amad1@gmail.com>
2023-02-06 03:45:22 +0200

    homepage hero section

--- src/app/site/pages/home-page/home-page.component.ts
+++ src/app/site/pages/home-page/home-page.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { LangService } from 'src/app/services/language.service';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.css'],
 })
-export class HomePageComponent {}
+export class HomePageComponent {
+  constructor(private _langService: LangService) {}
+  lang$ = this._langService.currentLang$;
+}
