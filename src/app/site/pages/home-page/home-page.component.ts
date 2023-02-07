cd637e8e0f6976478f0b16e881b2d238ae42b745
MohamedAlQadeery <moh9amad1@gmail.com>
2023-02-07 17:34:47 +0200

    fixes slider

--- src/app/site/pages/home-page/home-page.component.ts
+++ src/app/site/pages/home-page/home-page.component.ts
@@ -21,20 +21,20 @@ export class HomePageComponent {
     centeredSlides: false,
     infinite: true,
     speed: 300,
-    slidesToShow: 3,
+    slidesToShow: 4,
     slidesToScroll: 1,
     responsive: [
       {
         breakpoint: 992,
         settings: {
-          slidesToShow: 3,
+          slidesToShow: 4,
           slidesToScroll: 1,
         },
       },
       {
         breakpoint: 768,
         settings: {
-          slidesToShow: 2,
+          slidesToShow: 3,
           slidesToScroll: 1,
         },
       },
