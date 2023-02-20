67067f05f9c58f648b36b9e00f341264f2e2b2ce
MohamedAlQadeery <moh9amad1@gmail.com>
2023-02-20 23:15:57 +0200

    allowing interceptor to only work in domain requests

--- src/app/helpers/loader.interceptor.ts
+++ src/app/helpers/loader.interceptor.ts
@@ -7,15 +7,20 @@ import {
 } from '@angular/common/http';
 import { Observable, finalize } from 'rxjs';
 import { LoadingService } from '../services/loading.service';
+import { environment } from 'src/environments/environment';
 
 @Injectable()
 export class LoaderInterceptor implements HttpInterceptor {
   constructor(private _loadingService: LoadingService) {}
+  baseUrl = environment.baseURL;
 
   intercept(
     request: HttpRequest<unknown>,
     next: HttpHandler
   ): Observable<HttpEvent<unknown>> {
+    if (!request.url.includes(this.baseUrl)) {
+      return next.handle(request);
+    }
     this._loadingService.EmitLoader(true);
 
     return next.handle(request).pipe(
