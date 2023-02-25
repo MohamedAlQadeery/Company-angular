930a3d615700dc015137050b5a83a921a5f28a36
MohamedAlQadeery <moh9amad1@gmail.com>
2023-02-25 22:20:47 +0200

    validing offer images to be 1080x1080 and displaying it in offers page

--- src/app/shared/helpers/validation.ts
+++ src/app/shared/helpers/validation.ts
@@ -1,4 +1,10 @@
-import { AbstractControl, ValidatorFn } from '@angular/forms';
+import {
+  AbstractControl,
+  AsyncValidatorFn,
+  ValidationErrors,
+  ValidatorFn,
+} from '@angular/forms';
+import { Observable, from, of } from 'rxjs';
 
 export default class Validation {
   static match(controlName: string, checkControlName: string): ValidatorFn {
@@ -18,4 +24,36 @@ export default class Validation {
       }
     };
   }
+}
+
+// Define the dimensionsValidator function as before
+export function dimensionsValidator(
+  expectedWidth: number,
+  expectedHeight: number
+): AsyncValidatorFn {
+  return (control: AbstractControl): Observable<ValidationErrors | null> => {
+    const file = control.value;
+    if (!file) {
+      return of(null); // Return an Observable that emits null if no file is selected
+    }
+
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+
+    return from(
+      new Promise<ValidationErrors | null>((resolve) => {
+        reader.onload = () => {
+          const img = new Image();
+          img.onload = () => {
+            if (img.width === expectedWidth && img.height === expectedHeight) {
+              resolve(null); // The dimensions are valid, resolve with null
+            } else {
+              resolve({ dimensions: true }); // The dimensions are invalid, resolve with an error object
+            }
+          };
+          img.src = reader.result as string;
+        };
+      })
+    );
+  };
 }
